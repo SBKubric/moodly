35f53389eb940cc82a8e7d8c154e487125d5f37c
kupavel87 <admin@avftor.com>
2019-12-15 21:23:49 +0300

    Create checked_pages.txt

---
+++ vk.py
@@ -0,0 +1,91 @@
+import vk_api
+import re
+
+import local_settings
+
+stop_words = ['суицид']
+
+
+def search_url(text):
+    for word in text.split():
+        if 'id' in word:
+            id = re.search(r'\d+', word)
+            return id.group()
+    return False
+
+
+def search_stop_word(text):
+    for word in stop_words:
+        if word in text:
+            url = search_url(text)
+            if url:
+                return (word, url)
+            else:
+                return False
+    return False
+
+
+def vk_auth(login, password):
+    vk_session = vk_api.VkApi(login, password)
+
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+
+    vk = vk_session.get_api()
+    return vk
+
+
+def vk_group_search(vk, query):
+    '''Поиск групп по заданой строке'''
+    response = vk.groups.search(q=query, offset=0, count=50)
+    print(response['count'])
+    for item in response['items']:
+        print(f'{item["id"]} - {item["name"]}')
+
+
+def vk_wall_search(vk, owner_id):
+    '''Поиск по стене'''
+    response = vk.wall.get(owner_id=owner_id, offset=0, count=1)
+    wall_size = int(response['count'])
+    result = []
+    for index in range(0, wall_size, 100):
+        response = vk.wall.get(
+            owner_id=owner_id, offset=index, count=index + 100)
+        for item in response['items']:
+            temp = search_stop_word(item.get('text').lower())
+            if temp:
+                result.append(temp)
+        print('{} - {}'.format(index, len(result)))
+    with open('unchecked_pages.txt', 'w', encoding='utf-8') as f:
+        f.write('Количество: {}\n'.format(len(result)))
+        for item in result:
+            f.write(f'{item[0]} - {item[1]}\n')
+
+
+def check_pages(vk):
+    result = []
+    with open('unchecked_pages.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            result.append(line.split()[-1])
+    checked_result = []
+    for index in range(1, len(result), 50):
+        user_ids = ','.join(result[index:index + 50])
+        response = vk.users.get(user_ids=user_ids)
+        for user in response:
+            if user.get('can_access_closed'):
+                checked_result.append(user['id'])
+
+    with open('checked_pages.txt', 'w', encoding='utf-8') as f:
+        f.write('Количество: {}\n'.format(len(checked_result)))
+        for item in checked_result:
+            f.write(f'{item}\n')
+
+
+if __name__ == '__main__':
+    vk = vk_auth(local_settings.login, local_settings.password)
+    # groups = vk_group_search(vk, 'Дэд пейдж')
+    vk_wall_search(vk, -125339469)
+    check_pages(vk)
