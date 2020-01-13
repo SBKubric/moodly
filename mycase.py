aef75704d9c98f704c98dab20ac2d63eb16f493b
dwdSF <moralnoprosel@gmail.com>
2020-01-13 21:47:45 +0300

    Submissions in the form of dict are saved to a file.

--- mycase.py
+++ mycase.py
@@ -11,20 +11,30 @@ def reddit_auth():
 
 def main():
     subreddit_name = 'politics'              # Multiple subreddits can be combined with a + like so: 'politics+news'
-    user_search = 'Trump'                    
+    user_search = 'Iran'                    
     sort_type = 'relevance'                  # Can be one of: relevance, hot, top, new, comments.
     time = 'all'                             # Can be one of: all, day, hour, month, week, year.
     limit = None                             # None=all(Если не указывать лимит, вернет только 100)
     reddit = reddit_auth()
-    submissions(reddit, user_search, subreddit_name, sort_type, time, limit)
+    posts = submissions(reddit, user_search, subreddit_name, sort_type, time, limit)
+    post_output(posts)
+
+
+def post_output(posts):
+    with open('output.txt', 'w', encoding='utf-8') as f:
+        for post in posts:
+            f.write(f'{post}\n')
 
 
 def submissions(reddit, user_search, subreddit_name, sort_type, time, limit):
-    for num, submission in enumerate(reddit.subreddit(subreddit_name).search(user_search, sort=sort_type, time_filter=time, limit=limit), 1):
+    posts = []
+    for submission in reddit.subreddit(subreddit_name).search(user_search, sort=sort_type, time_filter=time, limit=limit):
         timestamp = submission.created_utc   # Time the submission was created, represented in Unix Time.
         value = datetime.datetime.fromtimestamp(timestamp)
         submission_time = value.strftime('%Y-%m-%d %H:%M:%S')
-        print(f'{num}:{submission.title}({submission_time})')
+        # print(f'{num}:[{submission.name}] {submission.title}({submission_time})')
+        posts.append({'name':submission.name,'title':submission.title,'time':submission_time})
+    return posts
 
     # for num, submission in enumerate(reddit.subreddit(subreddit_name).search(user_search, sort=sort_type, time_ filter=time, limit=limit), 1):
     #     print(f'{num} : {submission.title}\n{submission.selftext}')
